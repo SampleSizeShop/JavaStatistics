e7afd3164f43c1b96a60e8b9828cb5a2972c2645
Sarah Kreidler <sarah5280@gmail.com>
2010-01-18 23:45:05 +0000

    distribution for non-centrality parameter in a GLMM

---
+++ src/edu/cudenver/bios/distribution/GLMMNonCentrality.java
@@ -0,0 +1,70 @@
+package edu.cudenver.bios.distribution;
+
+import jsc.distributions.Distribution;
+
+/**
+ * Distribution of the non-centrality parameter in a general linear multivariate
+ * model 
+ *  
+ * @author Owner
+ *
+ */
+public class GLMMNonCentrality implements Distribution
+{
+
+	@Override
+	public double cdf(double arg0)
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double inverseCdf(double arg0)
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isDiscrete()
+	{
+		return false;
+	}
+
+	@Override
+	public double mean()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double pdf(double arg0)
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double random()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setSeed(long arg0)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public double variance()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
