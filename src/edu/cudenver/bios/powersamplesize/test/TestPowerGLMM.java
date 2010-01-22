edec7c0c5c661b35033df797eff182bfd392209d
Sarah Kreidler <sarah5280@gmail.com>
2010-01-22 23:50:01 +0000

    added tests for various combinations of uni/multivariate, fixed/random.  Still needs some cleanup

--- src/edu/cudenver/bios/powersamplesize/test/TestPowerGLMM.java
+++ src/edu/cudenver/bios/powersamplesize/test/TestPowerGLMM.java
@@ -15,192 +15,220 @@ import junit.framework.TestCase;
 
 public class TestPowerGLMM extends TestCase
 {
-    private static final int SIMULATION_SAMPLE_SIZE = 10000;
+    private static final int SIMULATION_SIZE = 10000;
     private static final double PRECISION = 0.01;
-    private static final double ALPHA = 0.05;
-    private static final int SAMPLE_SIZE = 50;
+    private static final double ALPHA = 0.05;    
+    private static final double MEAN = 9.75;
+    private static final double VARIANCE = 4.3;
+    private static final double[] BETA_SCALE = {0, 2.5, 0.5};
+    private static final double[] SIGMA_SCALE = {2};
+    private static final int[] SAMPLE_SIZE = {10, 20};
     
-    private static final String[] testNames = {"None", "Unirep", "W","PB", "HLT"}; 
-    
-    public void testValidUnivariateMatrix()
+    public void testValidUnivariateFixed()
     {
-        LinearModelPowerSampleSizeParameters goodParams = buildValidUnivariateInputs(false);
+        LinearModelPowerSampleSizeParameters goodParams = buildValidUnivariateInputs();
 
         PowerGLMM calc = new PowerGLMM();
-        int tests = 0; // number of tests run
-        int matches = 0; // number of matches between calculated and simulated power
-        for(TestStatistic statistic: TestStatistic.values())
-        {
-            if (statistic == TestStatistic.NONE) continue; // skip the "NONE" statistic for now
-            tests++;
-            System.out.println("Good inputs, Test: " + testNames[tests]);
-            
-            goodParams.setTestStatistic(statistic);
-            try
-            {
-                double calculated = calc.getCalculatedPower(goodParams);
-                double simulated = calc.getSimulatedPower((PowerSampleSizeParameters) goodParams, SIMULATION_SAMPLE_SIZE);
+        goodParams.setTestStatistic(TestStatistic.UNIREP);
+        checkPower("Valid Univariate, Fixed, UNIREP", calc, goodParams);
+        goodParams.setTestStatistic(TestStatistic.HOTELLING_LAWLEY_TRACE);
+        checkPower("Valid Univariate, Fixed, HLT",calc, goodParams);
+        goodParams.setTestStatistic(TestStatistic.PILLAU_BARTLETT_TRACE);
+        checkPower("Valid Univariate, Fixed, PB",calc, goodParams);
+        goodParams.setTestStatistic(TestStatistic.WILKS_LAMBDA);
+        checkPower("Valid Univariate, Fixed, W",calc, goodParams);        
+    }
+    
+    public void testInvalidUnivariateFixed()
+    {
+        LinearModelPowerSampleSizeParameters goodParams = buildValidUnivariateInputs();
+        goodParams.setBeta(null);
+        PowerGLMM calc = new PowerGLMM();
+        goodParams.setTestStatistic(TestStatistic.UNIREP);
+        checkPowerFail("Invalid Beta, Univariate, Fixed, UNIREP", calc, goodParams);
+  
+    }
+    
+    public void testValidMultivariateFixed()
+    {
+        LinearModelPowerSampleSizeParameters goodParams = buildValidMultivariateInputs();
 
-                System.out.println("Calculated power: " + calculated + ", simulated power: " + simulated);
-                if (Math.abs(simulated - calculated) < PRECISION) matches++;
-            }
-            catch(Exception e)
-            {
-                System.out.println("Failed to calculate power: " + e.getMessage());
-            }        
-        }
-        assertEquals(tests, matches);
+        PowerGLMM calc = new PowerGLMM();
+        goodParams.setTestStatistic(TestStatistic.UNIREP);
+        checkPower("Valid Univariate, Fixed, UNIREP", calc, goodParams);
+        goodParams.setTestStatistic(TestStatistic.HOTELLING_LAWLEY_TRACE);
+        checkPower("Valid Univariate, Fixed, HLT",calc, goodParams);
+        goodParams.setTestStatistic(TestStatistic.PILLAU_BARTLETT_TRACE);
+        checkPower("Valid Univariate, Fixed, PB",calc, goodParams);
+        goodParams.setTestStatistic(TestStatistic.WILKS_LAMBDA);
+        checkPower("Valid Univariate, Fixed, W",calc, goodParams);        
         
     }
     
-    public void testValidUnivariateEssenceMatrix()
+    public void testInvalidMultivariateFixed()
     {
-        LinearModelPowerSampleSizeParameters goodParams = buildValidUnivariateInputs(true);
+        LinearModelPowerSampleSizeParameters goodParams = buildValidMultivariateInputs();
 
+        goodParams.setSigmaError(null);
         PowerGLMM calc = new PowerGLMM();
-        int tests = 0; // number of tests run
-        int matches = 0; // number of matches between calculated and simulated power
-        for(TestStatistic statistic: TestStatistic.values())
-        {
-            if (statistic == TestStatistic.NONE) continue; // skip the "NONE" statistic for now
-            tests++;
-            System.out.println("Good inputs, Test: " + testNames[tests]);
-            
-            goodParams.setTestStatistic(statistic);
-            try
-            {
-                double calculated = calc.getCalculatedPower(goodParams);
-                double simulated = calc.getSimulatedPower((PowerSampleSizeParameters) goodParams, SIMULATION_SAMPLE_SIZE);
-
-                System.out.println("Calculated power: " + calculated + ", simulated power: " + simulated);
-                if (Math.abs(simulated - calculated) < PRECISION) matches++;
-            }
-            catch(Exception e)
-            {
-                System.out.println("Failed to calculate power: " + e.getMessage());
-            }        
-        }
-        assertEquals(tests, matches);
+        goodParams.setTestStatistic(TestStatistic.UNIREP);
+        checkPowerFail("Invalid Beta, Univariate, Fixed, UNIREP", calc, goodParams);
         
     }
     
-    public void testNonPositiveDefiniteDesign()
+    public void testValidUnivariateRandom()
     {
-        LinearModelPowerSampleSizeParameters goodParams = buildNonPositiveDefiniteInputs();
+        LinearModelPowerSampleSizeParameters goodParams = buildValidUnivariateInputs();     
+        
+    }
+    
+    public void testInvalidUnivariateRandom()
+    {
+        LinearModelPowerSampleSizeParameters goodParams = buildValidUnivariateInputs();
+
+
+        
+    }
+    
+    public void testValidMultivariateRandom()
+    {
+        LinearModelPowerSampleSizeParameters goodParams = buildValidMultivariateInputs();
+
+        ColumnMetaData randColMD = new ColumnMetaData();
+        randColMD.setPredictorType(PredictorType.RANDOM);
+        randColMD.setMean(MEAN);
+        randColMD.setVariance(VARIANCE);
+        goodParams.getDesignEssence().setColumnMetaData(2, new ColumnMetaData());
         PowerGLMM calc = new PowerGLMM();
+        goodParams.setTestStatistic(TestStatistic.UNIREP);
+        checkPower("Valid Univariate, Fixed, UNIREP", calc, goodParams);
+        goodParams.setTestStatistic(TestStatistic.HOTELLING_LAWLEY_TRACE);
+        checkPower("Valid Univariate, Fixed, HLT",calc, goodParams);
+        goodParams.setTestStatistic(TestStatistic.PILLAU_BARTLETT_TRACE);
+        checkPower("Valid Univariate, Fixed, PB",calc, goodParams);
+        goodParams.setTestStatistic(TestStatistic.WILKS_LAMBDA);
+        checkPower("Valid Univariate, Fixed, W",calc, goodParams);  
+
+
+        
+    }
+    
+    public void testInvalidMultivariateRandom()
+    {
+        LinearModelPowerSampleSizeParameters goodParams = buildValidUnivariateInputs();
+
+
+        
+    }
+    
+    private void checkPowerFail(String label, PowerGLMM calc, LinearModelPowerSampleSizeParameters params)
+    {
         try
         {
-            double calculated = calc.getCalculatedPower(goodParams);
-            double simulated = calc.getSimulatedPower((PowerSampleSizeParameters) goodParams, SIMULATION_SAMPLE_SIZE);
-            
-            assertEquals(calculated, simulated, PRECISION);
+            double calculated = calc.getCalculatedPower(params);
+            double simulated = calc.getSimulatedPower((PowerSampleSizeParameters) params, SIMULATION_SIZE);
+            assert(Math.abs(simulated - calculated) < PRECISION);
         }
         catch(Exception e)
         {
-            fail();
-        }   
-    }
-    
-    public void testNonConformingMatrix()
-    {
-        LinearModelPowerSampleSizeParameters goodParams = buildNonConformingInputs();
-        PowerGLMM calc = new PowerGLMM();
-        try
-        {
-            double calculated = calc.getCalculatedPower(goodParams);
-            double simulated = calc.getSimulatedPower((PowerSampleSizeParameters) goodParams, SIMULATION_SAMPLE_SIZE);
-            
-            assertEquals(calculated, simulated, PRECISION);
+            assert(true);
         }
-        catch(Exception e)
-        {
-            fail();
-        }   
+            
     }
     
-    private LinearModelPowerSampleSizeParameters buildValidUnivariateInputs(boolean essenceOnly)
+    private void checkPower(String label, PowerGLMM calc, LinearModelPowerSampleSizeParameters params)
+    {
+        int tests = 0; // number of tests run
+        int matches = 0; // number of matches between calculated and simulated power
+
+        for(int sampleSize: SAMPLE_SIZE)
+        {
+            for(double sigmaScale : SIGMA_SCALE)
+            {
+                for(double betaScale : BETA_SCALE)
+                {
+                    tests++;     
+                    LinearModelPowerSampleSizeParameters testParams = 
+                        new LinearModelPowerSampleSizeParameters(params);
+                    // scale the inputs
+                    testParams.setBeta(params.getBeta().scalarMultiply(betaScale));
+                    testParams.setSigmaError(params.getSigmaError().scalarMultiply(sigmaScale));
+                    testParams.setSampleSize(sampleSize);
+                    try
+                    {
+                        double calculated = calc.getCalculatedPower(testParams);
+                        double simulated = calc.getSimulatedPower(testParams, SIMULATION_SIZE);
+
+                        System.out.println(label + "["+betaScale+","+sigmaScale+","+sampleSize
+                                +"]>> Calculated power: " + calculated + ", simulated power: " + simulated);
+                        if (Math.abs(simulated - calculated) < PRECISION) matches++;
+                    }
+                    catch(Exception e)
+                    {
+                        System.out.println(label + "Failed to calculate power: " + e.getMessage());
+                    }        
+
+                }
+            }
+        }
+        assertEquals(tests, matches);
+    }
+    
+    private LinearModelPowerSampleSizeParameters buildValidUnivariateInputs()
     {
         LinearModelPowerSampleSizeParameters params = new LinearModelPowerSampleSizeParameters();
         params.setAlpha(ALPHA);
         
         // build beta matrix
-        double [][] beta = {{0},{2.0}};
+        double [][] beta = {{0},{1}};
         params.setBeta(new Array2DRowRealMatrix(beta));
         // build theta null matrix
         double [][] theta0 = {{0}};
         params.setTheta(new Array2DRowRealMatrix(theta0));
         // build sigma matrix
-        double [][] sigma = {{2.05}};
+        double [][] sigma = {{1}};
         params.setSigmaError(new Array2DRowRealMatrix(sigma));
         // build design matrix
-        if (!essenceOnly)
-        {
-            double [][] design = {
-                    {1,0},{1,0},{1,0},{1,0},{1,0},{1,0},{1,0},{1,0},{1,0},{1,0},
-                    {0,1},{0,1},{0,1},{0,1},{0,1},{0,1},{0,1},{0,1},{0,1},{0,1}};
-            params.setDesign(new Array2DRowRealMatrix(design));
-        }
-        else
-        {
-            double[][] essenceData = {{1,0},{0,1}};
-            EssenceMatrix essenceMatrix = new EssenceMatrix(essenceData);
-            essenceMatrix.setRowMetaData(0, new RowMetaData(6,1));
-            essenceMatrix.setRowMetaData(1, new RowMetaData(18,1));
-//            ColumnMetaData cmd = new ColumnMetaData();
-//            cmd.setMean(5);
-//            cmd.setVariance(3);
-//            cmd.setPredictorType(PredictorType.RANDOM);
-//            essenceMatrix.setColumnMetaData(1, cmd);
-            params.setDesignEssence(essenceMatrix);
-        }
+        double[][] essenceData = {{1,0},{0,1}};
+        EssenceMatrix essenceMatrix = new EssenceMatrix(essenceData);
+        essenceMatrix.setRowMetaData(0, new RowMetaData(10,1));
+        essenceMatrix.setRowMetaData(1, new RowMetaData(10,1));
+        params.setDesignEssence(essenceMatrix);
+
         // build between subject contrast
         double [][] between = {{1,-1}};
         params.setBetweenSubjectContrast(new Array2DRowRealMatrix(between));
         
         return params;     
-        
-    }
+    }   
+
     
-    private LinearModelPowerSampleSizeParameters buildNonConformingInputs()
+    private LinearModelPowerSampleSizeParameters buildValidMultivariateInputs()
     {
         LinearModelPowerSampleSizeParameters params = new LinearModelPowerSampleSizeParameters();
         params.setAlpha(ALPHA);
         
         // build beta matrix
-        
-        // build theta matrix
-        
+        double [][] beta = {{0},{1}, {1,2}};
+        params.setBeta(new Array2DRowRealMatrix(beta));
+        // build theta null matrix
+        double [][] theta0 = {{0}};
+        params.setTheta(new Array2DRowRealMatrix(theta0));
         // build sigma matrix
+        double [][] sigma = {{1}};
+        params.setSigmaError(new Array2DRowRealMatrix(sigma));
+        // build design matrix
+        double[][] essenceData = {{1,0,0},{0,1,0},{0,0,1}};
+        EssenceMatrix essenceMatrix = new EssenceMatrix(essenceData);
+        essenceMatrix.setRowMetaData(0, new RowMetaData(10,1));
+        essenceMatrix.setRowMetaData(1, new RowMetaData(10,1));
+        essenceMatrix.setRowMetaData(2, new RowMetaData(10,1));
+        params.setDesignEssence(essenceMatrix);
+
+        // build between subject contrast
+        double [][] between = {{1,-1}};
+        params.setBetweenSubjectContrast(new Array2DRowRealMatrix(between));
         
-        //
-        
-        return params;
-    }
-    
-    
-    private LinearModelPowerSampleSizeParameters buildNonPositiveDefiniteInputs()
-    {
-        LinearModelPowerSampleSizeParameters params = new LinearModelPowerSampleSizeParameters();
-        params.setAlpha(ALPHA);
-        
-        // build beta matrix
-        
-        // build theta matrix
-        
-        // build sigma matrix
-        
-        //
-        
-        return params;
-        
-    }
-    
-    private LinearModelPowerSampleSizeParameters buildMissingInputs()
-    {
-        LinearModelPowerSampleSizeParameters params = new LinearModelPowerSampleSizeParameters();
-        params.setAlpha(ALPHA);
-        params.setSampleSize(SAMPLE_SIZE);
-        
-        return params;
-    }
+        return params;     
+    }   
 }
