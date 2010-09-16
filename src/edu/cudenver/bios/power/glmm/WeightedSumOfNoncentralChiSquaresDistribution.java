54b0e00ade87fea168fd3236ad9afb1ed502a707
Sarah Kreidler <sarah5280@gmail.com>
2010-09-16 22:34:36 +0000

    added convergence factor code

--- src/edu/cudenver/bios/power/glmm/WeightedSumOfNoncentralChiSquaresDistribution.java
+++ src/edu/cudenver/bios/power/glmm/WeightedSumOfNoncentralChiSquaresDistribution.java
@@ -1,7 +1,11 @@
 package edu.cudenver.bios.power.glmm;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.TreeMap;
 
 /**
  * Distribution of a weighted sum of noncentral chi square variables
@@ -21,14 +25,14 @@ public class WeightedSumOfNoncentralChiSquaresDistribution
 	// maximum number of iterations allowed
 	private static final int MAX_STEPS = 200000;
 	// log(2) / 8 - no idea where this comes from, ask Davies
-	private static final double LN_2_DIV_8 = Math.log10(2.0) / 8.0;
+	private static final double LN_2_DIV_8 = Math.log(2.0) / 8.0;
 	
 	// parameters describing the weights (lambda), degrees of freedom (nu), 
 	// and non-centralities (omega) of the chi square variables
-	protected List<Double> lambdaList;
-	protected List<Double> degreesOfFreedomList;
-	protected List<Double> noncentralityList;
-
+	protected List<ChiSquareTerm> chiSquareTerms;
+	// map containing the ranks of the chi square terms (ranked by absolute value of lambda)
+	HashMap<Double, Integer> chiSquareRankMap = new HashMap<Double, Integer>();
+	
 	// min and max lambda values
 	protected double maxLambda;
 	protected double minLambda;
@@ -42,10 +46,52 @@ public class WeightedSumOfNoncentralChiSquaresDistribution
 	// simple counter class to maintain threadsafety for the iteration counter
 	private class Counter {
 		int count;
-		public Counter() { count = 0; }
-		public void increment() { count++; }
+		int maxSteps;
+		public Counter(int maxSteps) 
+		{ 
+			count = 0; 
+			this.maxSteps = maxSteps;
+		}
+		public void increment() 
+		{ 
+			count++; 
+			if (count > MAX_STEPS)
+				throw new RuntimeException("Exceeded max iterations");
+			}
 		public int getCount() { return count; }
 	};
+	
+	private class MinMaxComparator implements Comparator<ChiSquareTerm>
+	{
+		public MinMaxComparator() {};
+		@Override
+		public int compare(ChiSquareTerm cs1, ChiSquareTerm cs2)
+		{
+			if (cs1.getLambda() < cs2.getLambda())
+				return -1;
+			else if (cs1.getLambda() > cs2.getLambda())
+				return 1;
+			else
+				return 0;
+		}
+	}
+	
+	private class RankOrderComparator implements Comparator<Double>
+	{
+		@Override
+		public int compare(Double cs1, Double cs2)
+		{
+			double absLambda1 = Math.abs(cs1);
+			double absLambda2 = Math.abs(cs2);
+
+			if (absLambda1 < absLambda2)
+				return -1;
+			else if (absLambda1 > absLambda2)
+				return 1;
+			else
+				return 0;
+		}
+	}
 	
 	/**
 	 * Create a distribution for the specified weighted sum of non-central chi squares
@@ -57,28 +103,42 @@ public class WeightedSumOfNoncentralChiSquaresDistribution
 	 * @param accuracy maximum error allowed in probability
 	 * @throws IllegalArgumentException
 	 */
-	public WeightedSumOfNoncentralChiSquaresDistribution(List<Double> lambda, 
-			List<Double> nu, List<Double> omega, double normalCoefficient, double accuracy) 
+	public WeightedSumOfNoncentralChiSquaresDistribution(List<ChiSquareTerm> chiSquareTerms, 
+			double normalCoefficient, double accuracy) 
 	throws IllegalArgumentException
 	{
-		double N = lambda.size();
+		if (chiSquareTerms == null || chiSquareTerms.isEmpty())
+			throw new IllegalArgumentException("No chi-square terms specified");
+
+		if (Double.isNaN(normalCoefficient))
+			throw new IllegalArgumentException("Invalid coefficient for the normal term");
 		
-		// make sure all three lists are the same size
-		if (nu.size() != N || omega.size() != N)
-			throw new IllegalArgumentException("length of lambda, nu, and omega inputs must be equal to one another");
+		if (Double.isNaN(accuracy) || accuracy <= 0)
+			throw new IllegalArgumentException("Accuracy must be greater than 0");
 		
-		this.lambdaList = lambda;
-		this.degreesOfFreedomList = nu;
-		this.noncentralityList = omega;
+		this.chiSquareTerms = chiSquareTerms;
 		this.accuracy = accuracy;
 		this.normalCoefficient = normalCoefficient;
 		
-		maxLambda = (Double) Collections.max(lambdaList);
-		minLambda = (Double) Collections.min(lambdaList);
+		// find the min/max lambda  (truncate min at 0)
+		maxLambda = (Collections.max(chiSquareTerms, new MinMaxComparator())).getLambda();
+		minLambda = (Collections.min(chiSquareTerms, new MinMaxComparator())).getLambda();
+		if (minLambda > 0) minLambda = 0;
 		if (maxLambda < -minLambda) maxLambda = -minLambda;
 		
 		if (maxLambda == 0 && minLambda == 0 && normalCoefficient == 0)
 			throw new IllegalArgumentException("At least one of min/max lambda values or coefficient of the normal term must be non-zero");
+		
+		// Build a list of ranks for chi squares based on the absolute value of the lambdas
+		ArrayList<Double> sortedList = new ArrayList<Double>();
+		for(ChiSquareTerm chiSquare: this.chiSquareTerms) sortedList.add(chiSquare.getLambda());
+		Collections.sort(sortedList, new RankOrderComparator());
+		int rank = 0;
+		for(Double val: sortedList)
+		{
+			chiSquareRankMap.put(val, rank);
+			rank++;
+		}	
 	}
 	
 	/**
@@ -91,10 +151,10 @@ public class WeightedSumOfNoncentralChiSquaresDistribution
 	 * @param accuracy maximum error allowed in probability
 	 * @throws IllegalArgumentException
 	 */
-	public WeightedSumOfNoncentralChiSquaresDistribution(List<Double> lambda, 
-			List<Double> nu, List<Double> omega, double accuracy) throws IllegalArgumentException
+	public WeightedSumOfNoncentralChiSquaresDistribution(List<ChiSquareTerm> chiSquareTerms, 
+			double accuracy) throws IllegalArgumentException
 	{
-		this(lambda, nu, omega, DEFAULT_GAUSSIAN_COEFFICIENT, accuracy); 
+		this(chiSquareTerms, DEFAULT_GAUSSIAN_COEFFICIENT, accuracy); 
 	}
 	
 	/**
@@ -111,11 +171,7 @@ public class WeightedSumOfNoncentralChiSquaresDistribution
 	 */
 	public double cdf(double quantile)
 	throws RuntimeException
-	{
-		// Chi-square values must be positive, so probability is 0 
-		// for non-positive values TODO: double check this??
-		//if (quantile < 0) return 0;
-				
+	{				
 		// make sure at least one of min, max lambda and normal coefficient 
 		// are non-zero
 		if (minLambda == 0 && maxLambda == 0 && normalCoefficient == 0)
@@ -123,18 +179,19 @@ public class WeightedSumOfNoncentralChiSquaresDistribution
 		
 		double prob = 0;
 		// number of chi-square terms in the sum
-		double numChiSquareTerms = lambdaList.size(); 
+		double numChiSquareTerms = chiSquareTerms.size(); 
 		// expected value of ???
 		double mean = 0;
 		// initialize the standard deviation of the normal term
 		double sd = normalCoefficient * normalCoefficient;
 		
+		// 
 		// sum over something TODO: what?
-		for(int i = 0; i < numChiSquareTerms; i++)
+		for(ChiSquareTerm chiSquare: chiSquareTerms)
 		{
-			double lambda = lambdaList.get(i);
-			double df = degreesOfFreedomList.get(i);
-			double nonCentrality = noncentralityList.get(i);
+			double lambda = chiSquare.getLambda();
+			double df = chiSquare.getDegreesOfFreedom();
+			double nonCentrality = chiSquare.getNoncentrality();
 			
 			if (!(df < 0 || nonCentrality < 0))
 			{
@@ -159,15 +216,17 @@ public class WeightedSumOfNoncentralChiSquaresDistribution
 		sd = Math.sqrt(sd);
 
 		// initialize an interation counter  
-		Counter counter = new Counter();
+		Counter counter = new Counter(MAX_STEPS);
 
 		double halfAccuracy = 0.5 * accuracy;
 
 		double U = findTruncationPoint(16 / sd, halfAccuracy, counter);
 		
-		if (quantile == 0 || maxLambda <= 0.07 * sd)
+		if (quantile != 0 || maxLambda > 0.07 * sd)
 		{
-			// TODO: check if a covergence factor is helpful
+			// check if a covergence factor is helpful
+			double convergenceFactor = calculateConvergenceFactor(quantile, counter);
+			double TauSquared = 0.25*halfAccuracy/convergenceFactor; 
 		}
 		
 		// find the range of the distribution, determine if the quantile falls outside 
@@ -215,16 +274,16 @@ public class WeightedSumOfNoncentralChiSquaresDistribution
 			double sum2 = Math.abs(sum1);
 			double sum3 = -0.5*normalCoefficient*normalCoefficient*U*U;
 			
-			for(int j = lambdaList.size()-1; j >= 0; j--)
+			for(ChiSquareTerm chiSquare: chiSquareTerms)
 			{
-				double lambda = lambdaList.get(j);
-				double df = degreesOfFreedomList.get(j);
-				double omega = noncentralityList.get(j);
+				double lambda = chiSquare.getLambda();
+				double df = chiSquare.getDegreesOfFreedom();
+				double nonCentrality = chiSquare.getNoncentrality();
 				
 				double X = 2 * lambda * U;
 				double Y = X*X;
 				sum3 += -0.25*df*computeLog(Y, true);
-				Y = omega*X / (1+Y);
+				Y = nonCentrality*X / (1+Y);
 				double Z = df * Math.atan(X) + Y;
 				sum1 += Z;
 				sum2 += Math.abs(Z);
@@ -275,13 +334,13 @@ public class WeightedSumOfNoncentralChiSquaresDistribution
 		double[]  divisors = {2.0, 1.4, 1.2, 1.1}; 
 		// find the minimum U such that integration error will be below
 		// the specified accuracy
-		if (findIntegrationError(U, DEFAULT_TAU_SQUARED, counter) <= acc)
+		if (calculateIntegrationError(U, DEFAULT_TAU_SQUARED, counter) <= acc)
 		{
 			// our first guess was too high, so decrease U until we hit 
 			// the minimum value with appropriate accuracy
 			double Utemp = U;
 			U /= 4;
-			while(findIntegrationError(U, DEFAULT_TAU_SQUARED, counter) <= acc) 
+			while(calculateIntegrationError(U, DEFAULT_TAU_SQUARED, counter) <= acc) 
 			{
 				Utemp = U;
 				U /= 4;
@@ -292,22 +351,90 @@ public class WeightedSumOfNoncentralChiSquaresDistribution
 		{
 			// our first guess was too low, so increase U until we get
 			// good enough accuracy
-			U *= 4;
-			while(findIntegrationError(U, DEFAULT_TAU_SQUARED, counter) > acc) U *= 4;
+			do
+			{
+				U *= 4;
+			} 
+			while(calculateIntegrationError(U, DEFAULT_TAU_SQUARED, counter) > acc);
 		}
 		
 		// now ensure that the truncating error of U/1.2 > accuracy
+		double Utemp = U;
 		for(double divisor: divisors)
 		{
-			double Utemp = U / divisor;
-			if (findIntegrationError(Utemp, DEFAULT_TAU_SQUARED, counter) > acc)
-			{
-				U = Utemp;
+			Utemp /= divisor;
+			if (calculateIntegrationError(Utemp, DEFAULT_TAU_SQUARED, counter) > acc)
 				break;
-			}
+			U = Utemp;
 		}
 		
 		return U;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	private double calculateConvergenceFactor(double quantile, Counter counter)
+	throws RuntimeException
+	{
+		counter.increment();
+
+		// sign of the quantile (1, 0, or -1)
+		double quantileSign = 0;
+		if (quantile > 0)
+			quantileSign = 1;
+		else if (quantile < 0)
+		{
+			quantileSign = -1;
+		}
+		
+		double sum = 0;
+		double axl = Math.abs(quantile);
+
+		for(int j = chiSquareTerms.size()-1; j >= 0; j--)
+		{
+			ChiSquareTerm chiSquare = chiSquareTerms.get(j);
+			int rankIndex = this.chiSquareRankMap.get(chiSquare.getLambda());
+			ChiSquareTerm rankChiSquare = chiSquareTerms.get(rankIndex);
+		
+			double lambda = rankChiSquare.getLambda();
+			double lambdaAbsoluteValue = Math.abs(lambda);
+			double df = rankChiSquare.getDegreesOfFreedom();
+			double nonCentrality = rankChiSquare.getNoncentrality();
+			
+			if (lambda*quantileSign > 0)
+			{
+				double axl1 = axl - lambdaAbsoluteValue*(df + nonCentrality);
+				double axl2 = lambda / LN_2_DIV_8;
+				
+				if (axl1 > axl2)
+				{
+					axl = axl1;
+				}
+				else
+				{
+					if (axl > axl2) axl = axl2;
+					
+					sum = (axl - axl1)/lambda;
+					
+					for(int k = j-1; k >=0; k--)
+					{
+						ChiSquareTerm chiSquareK = chiSquareTerms.get(k);
+						int rankIndexK = this.chiSquareRankMap.get(chiSquareK.getLambda());
+						ChiSquareTerm rankChiSquareK = chiSquareTerms.get(rankIndexK);
+
+						sum += rankChiSquareK.getDegreesOfFreedom() + rankChiSquareK.getNoncentrality();
+					}
+					break;
+				}
+			}
+		}
+		
+//		if (sum > 100)
+//			throw new RuntimeException("Summation to big, bad roundoff error"); // TODO: do we need this check, another precision thing?
+//		
+		return Math.pow(2, sum/4) / (Math.PI*axl*axl);
 	}
 	
 	/**
@@ -316,12 +443,10 @@ public class WeightedSumOfNoncentralChiSquaresDistribution
 	 * 
 	 * @return integration error
 	 */
-	private double findIntegrationError(double U, double tauSquared, Counter counter)
+	private double calculateIntegrationError(double U, double tauSquared, Counter counter)
 	throws RuntimeException
 	{
 		counter.increment();
-		if (counter.getCount() > MAX_STEPS)
-			throw new RuntimeException("Exceeded max iterations");
 		
 		double sum1 = 0;
 		double sum2 = (normalCoefficient * normalCoefficient + tauSquared) * U * U;
@@ -331,15 +456,15 @@ public class WeightedSumOfNoncentralChiSquaresDistribution
 		double ns = 0;
 		double X = 0;
 		U *= 2;
-		double N = lambdaList.size();
-		for(int j = 0; j < N; j++)
+
+		for(ChiSquareTerm chiSquare: chiSquareTerms)
 		{
-			double df = degreesOfFreedomList.get(j);
-			double lambda = lambdaList.get(j);
-			double omega = noncentralityList.get(j);
+			double lambda = chiSquare.getLambda();
+			double df = chiSquare.getDegreesOfFreedom();
+			double nonCentrality = chiSquare.getNoncentrality();
 			
 			X = (U*lambda) * (U*lambda);
-			sum1 += omega * X / (1+X);
+			sum1 += nonCentrality * X / (1+X);
 			if (X <= 1)
 			{
 				prod1 += df * computeLog(X, true);
@@ -355,8 +480,8 @@ public class WeightedSumOfNoncentralChiSquaresDistribution
 		sum1 *= 0.5;
 		prod2 += prod1;
 		prod3 += prod1;
-		X = exponentiate(-sum1 - 0.25*prod2) / Math.PI;
-		double Y = exponentiate(-sum1 - 0.25*prod3) / Math.PI;
+		X = Math.exp(-sum1 - 0.25*prod2) / Math.PI;
+		double Y = Math.exp(-sum1 - 0.25*prod3) / Math.PI;
 		
 		// now that we've accumulated terms, build the three forms of
 		// the truncation error
@@ -428,12 +553,5 @@ public class WeightedSumOfNoncentralChiSquaresDistribution
 			return s;
 		}
 	}
-	
-	private double exponentiate(double x)
-	{
-		if (x < -706)
-			return 0; // TODO: is this an algol precision artifact?
-		else
-			return Math.exp(x);
-	}
+
 }
