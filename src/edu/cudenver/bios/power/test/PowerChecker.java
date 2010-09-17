2557fa28218c441d52759d952397ab9fbee1a232
Sarah Kreidler <sarah5280@gmail.com>
2010-09-17 19:44:05 +0000

    added flag to turn simulation on/off

--- src/edu/cudenver/bios/power/test/PowerChecker.java
+++ src/edu/cudenver/bios/power/test/PowerChecker.java
@@ -28,45 +28,80 @@ public class PowerChecker
     public static int checkPower(GLMMPowerParameters params, 
             boolean univariate, boolean fixed)
     {
-        // create a power calculator
-        GLMMPowerCalculator calc = new GLMMPowerCalculator();
+    	return PowerChecker.checkPower(params, univariate, fixed, true);   	
+    }
+    
+    /**
+     * Run the power calculations for the specified parameters and tests
+     * and assert whether they match simulation
+     * 
+     * @param params
+     * @param testList
+     * @returns the number of powers that failed to match simulation
+     */
+    public static int checkPower(GLMMPowerParameters params, 
+            boolean univariate, boolean fixed, boolean simulate)
+    {
+    	// create a power calculator
+    	GLMMPowerCalculator calc = new GLMMPowerCalculator();
 
-        // build prefix string
-        String uniStr = (univariate ? "U" : "M");
-        String fixedStr = (fixed ? "F" : "R");
+    	// build prefix string
+    	String uniStr = (univariate ? "U" : "M");
+    	String fixedStr = (fixed ? "F" : "R");
 
-        int matches = 0;
+    	int matches = 0;
 
-        System.out.println("Calculating power...");
-        List<Power> results = calc.getPower(params);
-        System.out.println("Done.");
-        System.out.println("Simulating power...");
-        List<Power> simResults = calc.getSimulatedPower(params, SIMULATION_SIZE);
-        System.out.println("Done.");
+    	System.out.println("Calculating power...");
+    	List<Power> results = calc.getPower(params);
+    	System.out.println("Done.");
+    	if (simulate)
+    	{
+    		System.out.println("Simulating power...");
+    		List<Power> simResults = calc.getSimulatedPower(params, SIMULATION_SIZE);
+    		System.out.println("Done.");
 
-        System.out.println("P-value\tM/U\tF/R\tCalc Power\tSim Power\tTest\tSigmaScale\tBetaScale\tTotal N\tAlpha\tPowerMethod\tQuantile");
-        for(int i = 0; i < results.size() && i < simResults.size(); i++)
-           // for(int i = 0; i < results.size(); i++)
+    		System.out.println("P-value\tM/U\tF/R\tCalc Power\tSim Power\tTest\tSigmaScale\tBetaScale\tTotal N\tAlpha\tPowerMethod\tQuantile");
 
-        {
-            GLMMPower power = (GLMMPower) results.get(i);
-            GLMMPower simPower = (GLMMPower) simResults.get(i);
-            double pValue = zTest(power.getActualPower(), simPower.getActualPower());
-            if (pValue > POWER_SIMULATION_COMPARISON_ALPHA) matches++;
+    		for(int i = 0; i < results.size(); i++)
+    		{
+    			GLMMPower power = (GLMMPower) results.get(i);
+    			GLMMPower simPower = (GLMMPower) simResults.get(i);
+    			double pValue = zTest(power.getActualPower(), simPower.getActualPower());
+    			if (pValue > POWER_SIMULATION_COMPARISON_ALPHA) matches++;
+    			System.out.println(Number.format(pValue) + "\t" + uniStr + "\t" + fixedStr + "\t" + 
+    					Number.format(power.getActualPower()) + "\t" +
+    					Number.format(simPower.getActualPower()) + "\t" +  
+    					power.getTest().toString() + "\t" + 
+    					Number.format(power.getSigmaScale()) + "\t" + 
+    					Number.format(power.getBetaScale()) + "\t" + 
+    					power.getTotalSampleSize() + "\t" + 
+    					Number.format(power.getAlpha()) + "\t" + 
+    					power.getPowerMethod() + "\t" + 
+    					power.getQuantile() + "\t");
+    		}
 
-            System.out.println(Number.format(pValue) + "\t" + uniStr + "\t" + fixedStr + "\t" + 
-            		Number.format(power.getActualPower()) + "\t" +
-            		Number.format(simPower.getActualPower()) + "\t" +  
-            		power.getTest().toString() + "\t" + 
-                    Number.format(power.getSigmaScale()) + "\t" + 
-                    Number.format(power.getBetaScale()) + "\t" + 
-                    power.getTotalSampleSize() + "\t" + 
-            		Number.format(power.getAlpha()) + "\t" + 
-                    power.getPowerMethod() + "\t" + 
-                    power.getQuantile() + "\t");
-        }
 
-        return results.size() - matches;
+    	}
+    	else
+    	{
+    		System.out.println("M/U\tF/R\tCalc Power\tTest\tSigmaScale\tBetaScale\tTotal N\tAlpha\tPowerMethod\tQuantile");
+    		for(int i = 0; i < results.size(); i++)
+    		{
+    			GLMMPower power = (GLMMPower) results.get(i);
+    			System.out.println(uniStr + "\t" + fixedStr + "\t" + 
+    					Number.format(power.getActualPower()) + "\t" +  
+    					power.getTest().toString() + "\t" + 
+    					Number.format(power.getSigmaScale()) + "\t" + 
+    					Number.format(power.getBetaScale()) + "\t" + 
+    					power.getTotalSampleSize() + "\t" + 
+    					Number.format(power.getAlpha()) + "\t" + 
+    					power.getPowerMethod() + "\t" + 
+    					power.getQuantile() + "\t");
+    		}
+    		matches = results.size();
+    	}
+
+    	return results.size() - matches;
     }
     
     /**
