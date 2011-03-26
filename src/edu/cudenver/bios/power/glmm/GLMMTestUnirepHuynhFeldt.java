5bf84a2e91dece2ba429a10999fb1614be7295a9
Sarah Kreidler <sarah5280@gmail.com>
2011-03-26 05:03:21 +0000

    implemented Muller, Edwards, Taylor method for calculating expected epsilon

--- src/edu/cudenver/bios/power/glmm/GLMMTestUnirepHuynhFeldt.java
+++ src/edu/cudenver/bios/power/glmm/GLMMTestUnirepHuynhFeldt.java
@@ -20,15 +20,7 @@
  */
 package edu.cudenver.bios.power.glmm;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import org.apache.commons.math.linear.EigenDecompositionImpl;
 import org.apache.commons.math.linear.RealMatrix;
-import org.apache.commons.math.linear.SingularValueDecompositionImpl;
-
-import edu.cudenver.bios.power.glmm.GLMMTest.FApproximation;
-import edu.cudenver.bios.power.glmm.GLMMTest.UnivariateCdfApproximation;
 
 /**
  * Implementation of the univariate approach to repeated measures test 
@@ -79,48 +71,70 @@ public class GLMMTestUnirepHuynhFeldt extends GLMMTestUnivariateRepeatedMeasures
     	super.calculateEpsilon();
     	double b = rankU;
 
-        // calculate the expected value of the epsilon estimate
-        // E[h(lambda)] = h(lambda) + g1 / (N - r)
-        // h(lambda) = h1(lambda) / (b*h2(lambda)
-        // see Muller, Barton (1989) for details
-        double h1 = totalN * sumLambda * sumLambda - 2 * sumLambdaSquared;
-        double h2 = (totalN - rank) * sumLambdaSquared - (sumLambda * sumLambda);
-        double g1 = 0;
-        for(int i = 0; i < distinctSigmaStarEigenValues.size(); i++)
-        {
-            EigenValueMultiplicityPair evmI = distinctSigmaStarEigenValues.get(i);
-            // derivatives of sub-equations comprising epsilon estimator
-            double h1firstDerivative = (2 * totalN * sumLambda) - (4 *evmI.eigenValue); 
-            double h1secondDerivative = 2 * totalN - 4;
+    	if (this.cdfMethod == UnivariateCdfApproximation.MULLER_BARTON_APPROX)
+    	{
+    		// calculate the expected value of the epsilon estimate
+    		// E[h(lambda)] = h(lambda) + g1 / (N - r)
+    		// h(lambda) = h1(lambda) / (b*h2(lambda)
+    		// see Muller, Barton (1989) for details
+    		double h1 = totalN * sumLambda * sumLambda - 2 * sumLambdaSquared;
+    		double h2 = (totalN - rank) * sumLambdaSquared - (sumLambda * sumLambda);
+    		double g1 = 0;
+    		for(int i = 0; i < distinctSigmaStarEigenValues.size(); i++)
+    		{
+    			EigenValueMultiplicityPair evmI = distinctSigmaStarEigenValues.get(i);
+    			// derivatives of sub-equations comprising epsilon estimator
+    			double h1firstDerivative = (2 * totalN * sumLambda) - (4 *evmI.eigenValue); 
+    			double h1secondDerivative = 2 * totalN - 4;
 
-            double h2firstDerivative = (2 * (totalN - rank) * evmI.eigenValue) - (2 * sumLambda); 
-            double h2secondDerivative = 2 * (totalN - rank) - 2;
+    			double h2firstDerivative = (2 * (totalN - rank) * evmI.eigenValue) - (2 * sumLambda); 
+    			double h2secondDerivative = 2 * (totalN - rank) - 2;
 
-            // derivatives of estimate of epsilon
-            double firstDerivative = ((h1firstDerivative) - ((h1 * h2firstDerivative) / h2)) / (h2 *b); 
-            double secondDerivative = 
-                (h1secondDerivative - ((2*h1firstDerivative*h2firstDerivative)/(h2)) +  
-                        (2 * h1 * h2firstDerivative * h2firstDerivative)/(h2*h2) - 
-                        (h1*h2secondDerivative)/(h2)) / 
-                        (h2*b);
+    			// derivatives of estimate of epsilon
+    			double firstDerivative = ((h1firstDerivative) - ((h1 * h2firstDerivative) / h2)) / (h2 *b); 
+    			double secondDerivative = 
+    				(h1secondDerivative - ((2*h1firstDerivative*h2firstDerivative)/(h2)) +  
+    						(2 * h1 * h2firstDerivative * h2firstDerivative)/(h2*h2) - 
+    						(h1*h2secondDerivative)/(h2)) / 
+    						(h2*b);
 
-            // accumulate the first term of g1 (sum over distinct eigen vals of 1st derivative * eigen val ^2 * multiplicity)
-            g1 += secondDerivative * evmI.eigenValue * evmI.eigenValue * evmI.multiplicity;
-            // loop over elements not equal to current
-            for(int j = 0; j < distinctSigmaStarEigenValues.size(); j++)
-            {
-                if (i != j)
-                {
-                    EigenValueMultiplicityPair evmJ = distinctSigmaStarEigenValues.get(j);
-                    // accumulate second term of g1
-                    g1 += ((firstDerivative * evmI.eigenValue * evmI.multiplicity * evmJ.eigenValue * evmJ.multiplicity) /
-                            (evmI.eigenValue - evmJ.eigenValue));
-                }
-            }
-        }
+    			// accumulate the first term of g1 (sum over distinct eigen vals of 1st derivative * eigen val ^2 * multiplicity)
+    			g1 += secondDerivative * evmI.eigenValue * evmI.eigenValue * evmI.multiplicity;
+    			// loop over elements not equal to current
+    			for(int j = 0; j < distinctSigmaStarEigenValues.size(); j++)
+    			{
+    				if (i != j)
+    				{
+    					EigenValueMultiplicityPair evmJ = distinctSigmaStarEigenValues.get(j);
+    					// accumulate second term of g1
+    					g1 += ((firstDerivative * evmI.eigenValue * evmI.multiplicity * evmJ.eigenValue * evmJ.multiplicity) /
+    							(evmI.eigenValue - evmJ.eigenValue));
+    				}
+    			}
+    		}
 
-        expectedEpsilon = (totalN*b*epsilonD - 2)/(b*(totalN - rank - b*epsilonD))  + g1 / (totalN - rank);
+    		expectedEpsilon = (totalN*b*epsilonD - 2)/(b*(totalN - rank - b*epsilonD))  + g1 / (totalN - rank);
+    	}
+    	else
+    	{
+    		double sum = 0;
+			for(int i = 0; i < sigmaStarEigenValues.length; i++)
+			{
+				for(int j = 0; j < sigmaStarEigenValues.length; j++)
+				{
+					sum += sigmaStarEigenValues[i] * sigmaStarEigenValues[j];
+				}
+			}
 
+			double nu = totalN - rank;
+			double expT1 = (2 * nu * sumLambdaSquared) + (nu * nu * sumLambda * sumLambda);
+			double expT2 = ((nu * (nu + 1)) * sumLambdaSquared) + (nu * sum);
+			
+			double numerator = (1/b) * (((nu + 1)*expT1) - 2 * expT2);
+			double denominator = (nu * expT2) - expT1;
+			
+			expectedEpsilon = numerator / denominator;
+    	}      
 
         // ensure that expected value is within bounds 1/b to 1
         if (expectedEpsilon != Double.NaN)
