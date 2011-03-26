ff9b297fa02d158145afdf53d6b1873a6dc18970
Sarah Kreidler <sarah5280@gmail.com>
2011-03-26 05:11:45 +0000

    re-added Muller Barton approximation

--- src/edu/cudenver/bios/power/glmm/GLMMTestUnirepGeisserGreenhouse.java
+++ src/edu/cudenver/bios/power/glmm/GLMMTestUnirepGeisserGreenhouse.java
@@ -86,70 +86,76 @@ public class GLMMTestUnirepGeisserGreenhouse extends GLMMTestUnivariateRepeatedM
     {          
     	super.calculateEpsilon();
 
-//         calculate the expected value of the epsilon estimate
-//         E[f(lambda)] = f(lambda) + g1 / (N - r)
-//         see Muller, Barton (1989) for details
-//        double g1 = 0;
-//        for(int i = 0; i < distinctEigenValues.size(); i++)
-//        {
-//            EigenValueMultiplicityPair evmI = distinctEigenValues.get(i);
-//            double firstDerivative = 
-//                ((2 * sumLambda)/(b * sumLambdaSquared) - 
-//                        (2 * evmI.eigenValue * sumLambda * sumLambda)/(b*sumLambdaSquared*sumLambdaSquared));
-//
-//            double secondDerivative = 
-//                (2 / (b * sumLambdaSquared) - 
-//                        (8*evmI.eigenValue*sumLambda)/(b*sumLambdaSquared*sumLambdaSquared) + 
-//                        (8*evmI.eigenValue*evmI.eigenValue*sumLambda*sumLambda)/(b*sumLambdaSquared*sumLambdaSquared*sumLambdaSquared) - 
-//                        (2*sumLambda*sumLambda)/(b*sumLambdaSquared*sumLambdaSquared)); 
-//
-//            // accumulate the first term of g1 (sum over distinct eigen vals of 1st derivative * eigen val ^2 * multiplicity)
-//            g1 += secondDerivative * evmI.eigenValue * evmI.eigenValue * evmI.multiplicity;
-//            // loop over elements not equal to current
-//            for(int j = 0; j < distinctEigenValues.size(); j++)
-//            {
-//                if (i != j)
-//                {
-//                    EigenValueMultiplicityPair evmJ = distinctEigenValues.get(j);
-//                    // accumulate second term of g1
-//                    g1 += ((firstDerivative * evmI.eigenValue * evmI.multiplicity * evmJ.eigenValue * evmJ.multiplicity) /
-//                            (evmI.eigenValue - evmJ.eigenValue));
-//                }
-//            }
-//        }
-//
-//        this.unirepEpsilonExpectedValue = unirepEpsilon  + g1 / (N - r);
+    	if (this.cdfMethod == UnivariateCdfApproximation.MULLER_BARTON_APPROX)
+    	{
+    		// calculate the expected value of the epsilon estimate
+    		//  E[f(lambda)] = f(lambda) + g1 / (N - r)
+    		//  see Muller, Barton (1989) for details
+    		double b = rankU;
+    		double g1 = 0;
+    		for(int i = 0; i < distinctSigmaStarEigenValues.size(); i++)
+    		{
+    			EigenValueMultiplicityPair evmI = distinctSigmaStarEigenValues.get(i);
+    			double firstDerivative = 
+    				((2 * sumLambda)/(b * sumLambdaSquared) - 
+    						(2 * evmI.eigenValue * sumLambda * sumLambda)/(b*sumLambdaSquared*sumLambdaSquared));
 
-        // calculate the expected value of the epsilon estimate
-        // see Muller, Edwards, Taylor (2004) for details
-        
-        // build a vector with each eigen value repeated per its multiplicity
-        ArrayRealVector eigenColumnVector = new ArrayRealVector();
-        for(EigenValueMultiplicityPair evmp: distinctSigmaStarEigenValues) 
-        {
-            // there is probably a more memory-efficient method to do this. 
-            eigenColumnVector = eigenColumnVector.append(new ArrayRealVector((int) evmp.multiplicity, evmp.eigenValue));
-        }
+    			double secondDerivative = 
+    				(2 / (b * sumLambdaSquared) - 
+    						(8*evmI.eigenValue*sumLambda)/(b*sumLambdaSquared*sumLambdaSquared) + 
+    						(8*evmI.eigenValue*evmI.eigenValue*sumLambda*sumLambda)/(b*sumLambdaSquared*sumLambdaSquared*sumLambdaSquared) - 
+    						(2*sumLambda*sumLambda)/(b*sumLambdaSquared*sumLambdaSquared)); 
 
-        RealMatrix outerProduct = eigenColumnVector.outerProduct(eigenColumnVector);
-        double sum = outerProduct.walkInOptimizedOrder(new SummationVisitor());
-        double nu = (totalN - rank);
-        double expT1 = (2*nu*sumLambdaSquared) + (nu*nu*sumLambda*sumLambda);
-        double expT2 = (nu*(nu + 1)*sumLambdaSquared) + (nu*sum*sum);
-        expectedEpsilon = (1/(double)rankU)*(expT1/expT2);
-  
-        // ensure that expected value is within bounds 1/b to 1
-        if (expectedEpsilon != Double.NaN)
-        {
-            if (expectedEpsilon < 1/rankU)
-            {
-            	expectedEpsilon = 1/rankU;
-            }
-            else if (expectedEpsilon > 1)
-            {
-            	expectedEpsilon = 1;
-            }
-        }
+    			// accumulate the first term of g1 (sum over distinct eigen vals of 1st derivative * eigen val ^2 * multiplicity)
+    			g1 += secondDerivative * evmI.eigenValue * evmI.eigenValue * evmI.multiplicity;
+    			// loop over elements not equal to current
+    			for(int j = 0; j < distinctSigmaStarEigenValues.size(); j++)
+    			{
+    				if (i != j)
+    				{
+    					EigenValueMultiplicityPair evmJ = distinctSigmaStarEigenValues.get(j);
+    					// accumulate second term of g1
+    					g1 += ((firstDerivative * evmI.eigenValue * evmI.multiplicity * evmJ.eigenValue * evmJ.multiplicity) /
+    							(evmI.eigenValue - evmJ.eigenValue));
+    				}
+    			}
+    		}
+
+    		expectedEpsilon = epsilonD  + g1 / (totalN - rank);
+
+    	}
+    	else
+    	{
+    		// calculate the expected value of the epsilon estimate
+    		// see Muller, Edwards, Taylor (2004) for details
+
+    		// build a vector with each eigen value repeated per its multiplicity
+    		ArrayRealVector eigenColumnVector = new ArrayRealVector();
+    		for(EigenValueMultiplicityPair evmp: distinctSigmaStarEigenValues) 
+    		{
+    			// there is probably a more memory-efficient method to do this. 
+    			eigenColumnVector = eigenColumnVector.append(new ArrayRealVector((int) evmp.multiplicity, evmp.eigenValue));
+    		}
+
+    		RealMatrix outerProduct = eigenColumnVector.outerProduct(eigenColumnVector);
+    		double sum = outerProduct.walkInOptimizedOrder(new SummationVisitor());
+    		double nu = (totalN - rank);
+    		double expT1 = (2*nu*sumLambdaSquared) + (nu*nu*sumLambda*sumLambda);
+    		double expT2 = (nu*(nu + 1)*sumLambdaSquared) + (nu*sum*sum);
+    		expectedEpsilon = (1/(double)rankU)*(expT1/expT2);
+    	}
+    	// ensure that expected value is within bounds 1/b to 1
+    	if (expectedEpsilon != Double.NaN)
+    	{
+    		if (expectedEpsilon < 1/rankU)
+    		{
+    			expectedEpsilon = 1/rankU;
+    		}
+    		else if (expectedEpsilon > 1)
+    		{
+    			expectedEpsilon = 1;
+    		}
+    	}
     }
     
     /**
