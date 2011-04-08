b11c8315f7b756bad323648ca7af4b6f4c4dfccf
Sarah Kreidler <sarah5280@gmail.com>
2011-04-08 21:50:37 +0000

    added batch file to run all of the sas unit tests from the command line

---
+++ sas/code/runall.bat
@@ -0,0 +1,23 @@
+REM
+REM This file runs all SAS code which generates XML inputs to the java unit tests
+REM
+"C:\Program Files\SAS\SASFoundation\9.2\sas.exe" -NOSPLASH -NOLOG -NOPRINT -NOLOG -NOPRINT -sysin ./TestConditionalMultivariate.sas
+"C:\Program Files\SAS\SASFoundation\9.2\sas.exe" -NOSPLASH -NOLOG -NOPRINT -NOLOG -NOPRINT -sysin ./TestConditionalMultivariateInteraction.sas
+"C:\Program Files\SAS\SASFoundation\9.2\sas.exe" -NOSPLASH -NOLOG -NOPRINT -NOLOG -NOPRINT -sysin ./TestConditionalMultivariateWithConfidenceLimits.sas
+"C:\Program Files\SAS\SASFoundation\9.2\sas.exe" -NOSPLASH -NOLOG -NOPRINT -sysin ./TestConditionalOrthogonalPolynomial1Factor.sas
+"C:\Program Files\SAS\SASFoundation\9.2\sas.exe" -NOSPLASH -NOLOG -NOPRINT -sysin ./TestConditionalOrthogonalPolynomial2Factor.sas
+"C:\Program Files\SAS\SASFoundation\9.2\sas.exe" -NOSPLASH -NOLOG -NOPRINT -sysin ./TestConditionalOrthogonalPolynomial3Factor.sas
+"C:\Program Files\SAS\SASFoundation\9.2\sas.exe" -NOSPLASH -NOLOG -NOPRINT -sysin ./TestConditionalPairedTTest.sas
+"C:\Program Files\SAS\SASFoundation\9.2\sas.exe" -NOSPLASH -NOLOG -NOPRINT -sysin ./TestConditionalTwoSampleTTest.sas
+"C:\Program Files\SAS\SASFoundation\9.2\sas.exe" -NOSPLASH -NOLOG -NOPRINT -sysin ./TestConditionalTwoSampleTTest3DPlot.sas
+"C:\Program Files\SAS\SASFoundation\9.2\sas.exe" -NOSPLASH -NOLOG -NOPRINT -sysin ./TestConditionalUnivariate.sas
+"C:\Program Files\SAS\SASFoundation\9.2\sas.exe" -NOSPLASH -NOLOG -NOPRINT -sysin ./TestConditionalUnivariateWithConfidenceLimits.sas
+"C:\Program Files\SAS\SASFoundation\9.2\sas.exe" -NOSPLASH -NOLOG -NOPRINT -sysin ./TestDaviesAlgorithm.sas
+"C:\Program Files\SAS\SASFoundation\9.2\sas.exe" -NOSPLASH -NOLOG -NOPRINT -sysin ./TestHotellingLawleyApproximateQuantile.sas
+"C:\Program Files\SAS\SASFoundation\9.2\sas.exe" -NOSPLASH -NOLOG -NOPRINT -sysin ./TestHotellingLawleyApproximateUnconditional.sas
+"C:\Program Files\SAS\SASFoundation\9.2\sas.exe" -NOSPLASH -NOLOG -NOPRINT -sysin ./TestHotellingLawleyExactQuantile.sas
+"C:\Program Files\SAS\SASFoundation\9.2\sas.exe" -NOSPLASH -NOLOG -NOPRINT -sysin ./TestHotellingLawleyExactUnconditional.sas
+"C:\Program Files\SAS\SASFoundation\9.2\sas.exe" -NOSPLASH -NOLOG -NOPRINT -sysin ./TestUnirepApproximateQuantile.sas
+"C:\Program Files\SAS\SASFoundation\9.2\sas.exe" -NOSPLASH -NOLOG -NOPRINT -sysin ./TestUnirepApproximateUnconditional.sas
+"C:\Program Files\SAS\SASFoundation\9.2\sas.exe" -NOSPLASH -NOLOG -NOPRINT -sysin ./TestUnirepExactQuantile.sas
+"C:\Program Files\SAS\SASFoundation\9.2\sas.exe" -NOSPLASH -NOLOG -NOPRINT -sysin ./TestUnirepExactUnconditional.sas
