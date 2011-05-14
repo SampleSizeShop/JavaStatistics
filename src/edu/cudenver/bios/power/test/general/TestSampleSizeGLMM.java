59fe5c5ff895d2df1c94fd958290268183aa276b
Sarah Kreidler <sarah5280@gmail.com>
2011-05-14 00:20:50 +0000

    removed excess test case, updated TestSampleSizeGLMM to include a GLMM(F,g)

--- src/edu/cudenver/bios/power/test/general/TestSampleSizeGLMM.java
+++ src/edu/cudenver/bios/power/test/general/TestSampleSizeGLMM.java
@@ -23,22 +23,19 @@ package edu.cudenver.bios.power.test.general;
 import java.text.DecimalFormat;
 import java.util.List;
 
+import jsc.distributions.Normal;
+import junit.framework.TestCase;
+
 import org.apache.commons.math.linear.Array2DRowRealMatrix;
 import org.apache.commons.math.linear.MatrixUtils;
-import org.apache.commons.math.linear.RealMatrix;
-import edu.cudenver.bios.matrix.DesignEssenceMatrix;
+
 import edu.cudenver.bios.matrix.FixedRandomMatrix;
-import edu.cudenver.bios.matrix.RandomColumnMetaData;
-import edu.cudenver.bios.matrix.RowMetaData;
 import edu.cudenver.bios.power.GLMMPower;
 import edu.cudenver.bios.power.GLMMPowerCalculator;
 import edu.cudenver.bios.power.Power;
 import edu.cudenver.bios.power.glmm.GLMMTestFactory.Test;
 import edu.cudenver.bios.power.parameters.GLMMPowerParameters;
 import edu.cudenver.bios.power.parameters.GLMMPowerParameters.PowerMethod;
-
-import jsc.distributions.Normal;
-import junit.framework.TestCase;
 
 /**
  * Non-automated test for GLMM sample size
@@ -47,25 +44,14 @@ import junit.framework.TestCase;
  */
 public class TestSampleSizeGLMM extends TestCase
 {
-    private static final int SIMULATION_SIZE = 10000;
-    private static final double UNIT_TEST_ALPHA = 0.01;
-    private static final double MEAN = 9.75;
     private static final double VARIANCE = 2.0;
     private static final double[] ALPHA_LIST = {0.05};    
     private static final double[] BETA_SCALE_LIST = {0,1,2};
     private static final double[] SIGMA_SCALE_LIST = {1,2};
-    private static final double[] POWER_LIST = {0.7,0.8,0.9};
-    private Normal normalDist;
-    private DecimalFormat Number;
-
-    public void setUp()
-    {
-        normalDist = new Normal();
-        Number = new DecimalFormat("#0.000");
-
-    }
+    private static final double[] POWER_LIST = {0.05,0.7,0.8,0.9};
+    private DecimalFormat Number = new DecimalFormat("#0.000");
     
-    private void testValidUnivariateFixed()
+    public void testValidUnivariateFixed()
     {
         // build the inputs
         GLMMPowerParameters params = buildValidUnivariateInputs();
@@ -73,33 +59,21 @@ public class TestSampleSizeGLMM extends TestCase
         GLMMPowerCalculator calc = new GLMMPowerCalculator();
                 
         List<Power> results = calc.getSampleSize(params);
-        //List<Power> simResults = calc.getSimulatedPower(params, SIMULATION_SIZE);
-        System.out.println("Multi?\tFixed?\tAlpha\tSigmaScale\tBetaScale\tTotal N\tPower");
-        for(Power power: results)
-        {
-        	GLMMPower p = (GLMMPower) power;
-        	System.out.println("U\tF\t" + p.getTest().toString() + "\t" + Number.format(p.getAlpha()) +
-        			"\t" + Number.format(p.getSigmaScale()) + "\t" + Number.format(p.getBetaScale()) + 
-        			"\t" + p.getTotalSampleSize() + "\t" + Number.format(p.getActualPower()));
-        }
+        checkSampleSize(false, results);
     }
 
-    private void testInvalidUnivariateFixed()
+    public void testInvalidUnivariateFixed()
     {
         // build the inputs
         GLMMPowerParameters params = buildValidUnivariateInputs();
         // create a power calculator
         GLMMPowerCalculator calc = new GLMMPowerCalculator();
         params.setBeta(null);
-
         try
         {
         	List<Power> results = calc.getSampleSize(params);
-        	//List<Power> simResults = calc.getSimulatedPower(params, SIMULATION_SIZE);
-        	for(Power p: results)
-        	{
-        		System.out.println("Univariate, Fixed: " + p.toXML());
-        	}
+        	checkSampleSize(false, results);
+        	fail();
         }
         catch (Exception e)
         {
@@ -115,18 +89,10 @@ public class TestSampleSizeGLMM extends TestCase
         GLMMPowerCalculator calc = new GLMMPowerCalculator();
         
         List<Power> results = calc.getSampleSize(params);
-        System.out.println("Multi?\tFixed?\tAlpha\tSigmaScale\tBetaScale\tTotal N\tPower");
-        for(Power power: results)
-        {
-        	GLMMPower p = (GLMMPower) power;
-        	System.out.println("M\tF\t" + p.getTest().toString() + "\t" + Number.format(p.getAlpha()) +
-        			"\t" + Number.format(p.getSigmaScale()) + "\t" + Number.format(p.getBetaScale()) + 
-        			"\t" + p.getTotalSampleSize() + "\t" + Number.format(p.getActualPower()));
-        }
-
+        checkSampleSize(false, results);
     }
 
-    private void testInvalidMultivariateFixed()
+    public void testInvalidMultivariateFixed()
     {
         // build the inputs
         GLMMPowerParameters params = buildValidMultivariateFixedInputs();
@@ -137,11 +103,8 @@ public class TestSampleSizeGLMM extends TestCase
         try
         {
                 List<Power> results = calc.getSampleSize(params);
-                //List<Power> simResults = calc.getSimulatedPower(params, SIMULATION_SIZE);
-                for(Power p: results)
-                {
-                    System.out.println("Multivariate, Fixed: " + p.toXML());
-                }
+                checkSampleSize(false, results);
+                fail();
         }
         catch (Exception e)
         {
@@ -149,28 +112,72 @@ public class TestSampleSizeGLMM extends TestCase
         }
     }
 
-    private void testValidMultivariateRandom()
+    public void testValidMultivariateRandom()
     {
-        // build the inputs
-        GLMMPowerParameters params = buildValidMultivariateRandomInputs();
-        
-        // create a power calculator
-        GLMMPowerCalculator calc = new GLMMPowerCalculator();
-        
-        List<Power> results = calc.getSampleSize(params);
-        //List<Power> simResults = calc.getSimulatedPower(params, SIMULATION_SIZE);
+		// build the inputs
+		double[] beta5 = {
+				0.4997025,
+				0.8075886,
+				1.097641};
+		GLMMPowerParameters params5 = buildValidMultivariateRandomInputs(beta5, 5);
+		double[] beta25 = {
+				0.1651525,
+				0.2623301, 
+				0.3508015
+		};
+		GLMMPowerParameters params25 = buildValidMultivariateRandomInputs(beta25, 25);
+		double[] beta50 = {
+				0.1141548,
+				0.1812892,
+				0.2423835
+		};
+		GLMMPowerParameters params50 = buildValidMultivariateRandomInputs(beta50, 50);
+
+		GLMMPowerCalculator calc = new GLMMPowerCalculator();
+		List<Power> sampleSizeList = calc.getSampleSize(params50); 
+        checkSampleSize(true, sampleSizeList);
+    }
+
+    public void testInvalidMultivariateRandom()
+    {
+
+    }
+
+    private void checkSampleSize(boolean isGlmmFG, List<Power> results)
+    {
+        int invalidSampleSize = 0;
         for(Power p: results)
         {
-        	System.out.println("Multivariate, Random: " + p.toXML());
+        	if (p.getActualPower() < p.getNominalPower()) invalidSampleSize++;
         }
+        outputResults(isGlmmFG, results);
+        assertEquals(invalidSampleSize, 0);
+        
     }
 
-    private void testInvalidMultivariateRandom()
+    private void outputResults(boolean isGlmmFG, List<Power> resultList)
     {
-
+    	System.out.println("Calc Power (lower, upper)\tTest\tSigmaScale\tBetaScale\tTotal N\tAlpha\tPowerMethod\tQuantile");
+    	
+    	for(Power power: resultList)
+    	{
+    		GLMMPower result = (GLMMPower) power;
+    		System.out.println(Number.format(result.getActualPower()) + "(" + 
+    				(result.getConfidenceInterval() != null ? 
+    						Number.format(result.getConfidenceInterval().getLowerLimit()) : "n/a") + ", " + 
+    				(result.getConfidenceInterval() != null ? 
+    						Number.format(result.getConfidenceInterval().getUpperLimit()) : "n/a") + ")\t" +  
+    				result.getTest() + "\t" + 
+    				Number.format(result.getSigmaScale()) + "\t" + 
+    				Number.format(result.getBetaScale()) + "\t" + 
+    				result.getTotalSampleSize() + "\t" + 
+    				Number.format(result.getAlpha()) + "\t" + 
+    				result.getPowerMethod() + "\t" + 
+    				result.getQuantile() + "\t");
+    	}
     }
-
-
+    
+    
     /********** helper functions to create the matrices ***********/
     
     /**
@@ -343,14 +350,68 @@ public class TestSampleSizeGLMM extends TestCase
         return params;     
     }
     
-    private boolean powersAreSame(double calc, double sim)
-    {
-        double z = Math.abs((sim - calc) / Math.sqrt((sim * (1 - sim)) / SIMULATION_SIZE));
-        
-        double p = 2 * normalDist.upperTailProb(z);
-        System.out.println("(p = " + Number.format(p) + ")");
-        return (p > UNIT_TEST_ALPHA);
-    } 
-    
+	/**
+	 * Builds matrices for a multivariate GLM with a baseline covariate
+	 * Note, this matrix set matches the values produced in Table II from Glueck&Muller
+	 */
+	private GLMMPowerParameters buildValidMultivariateRandomInputs(double[] betaScaleList, int repn)
+	{
+		GLMMPowerParameters params = new GLMMPowerParameters();
+		params.addPower(0.9);
+		// add quantile power method
+		params.addPowerMethod(PowerMethod.QUANTILE_POWER);
+		params.addQuantile(0.5);
+		
+		// add HLT as the statistical test
+		params.addTest(Test.HOTELLING_LAWLEY_TRACE);
+
+		// add alpha values
+		for(double alpha: ALPHA_LIST) params.addAlpha(alpha);
+
+		int P = 3;
+		int Q = 3;
+		// create design matrix
+		params.setDesignEssence(MatrixUtils.createRealIdentityMatrix(Q));
+		// add sample size multipliers
+		//  for(int sampleSize: SAMPLE_SIZE_LIST) params.addSampleSize(sampleSize);
+		params.addSampleSize(repn);
+		// build sigma G matrix
+		double[][] sigmaG = {{1}};
+		params.setSigmaGaussianRandom(new Array2DRowRealMatrix(sigmaG));
+
+		// build sigma Y matrix
+		double rho = 0.4;
+		double [][] sigmaY = {{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}};
+		params.setSigmaOutcome(new Array2DRowRealMatrix(sigmaY));
+
+		// build sigma YG
+		double [][] sigmaYG = {{0.5},{0.5}, {0.5}, {0}};
+		params.setSigmaOutcomeGaussianRandom(new Array2DRowRealMatrix(sigmaYG));
+
+		// add sigma scale values
+		for(double sigmaScale: SIGMA_SCALE_LIST) params.addSigmaScale(sigmaScale);
+
+		// build beta matrix
+		double [][] beta = {{1,0,0,0},{0,2,0,0},{0,0,0,0}};
+		double [][] betaRandom = {{1,1,1,1}};
+		params.setBeta(new FixedRandomMatrix(beta, betaRandom, false));
+		// add beta scale values
+		for(double betaScale: betaScaleList) params.addBetaScale(betaScale);
+
+		// build theta null matrix
+		double [][] theta0 = {{0,0,0,0},{0,0,0,0}};
+		params.setTheta(new Array2DRowRealMatrix(theta0));
+
+		// build between subject contrast
+		double [][] between = {{-1,1,0}, {-1,0,1}};
+		double[][] betweenRandom = {{0}, {0}};
+		params.setBetweenSubjectContrast(new FixedRandomMatrix(between, betweenRandom, true));
+
+		// build within subject contrast
+		double [][] within = {{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}};
+		params.setWithinSubjectContrast(new Array2DRowRealMatrix(within));
+
+		return params;     
+	}
 }
 
