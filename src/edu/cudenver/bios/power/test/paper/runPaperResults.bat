3e5f9d73d74aa68d9dd2ccf5470d91dd54d1d7be
Sarah Kreidler <sarah5280@gmail.com>
2011-06-22 22:33:50 +0000

    updated classpath for new GLIMMPSEresults bundle

--- src/edu/cudenver/bios/power/test/paper/runPaperResults.bat
+++ src/edu/cudenver/bios/power/test/paper/runPaperResults.bat
@@ -1,5 +1,5 @@
 REM
 REM This file runs all Junit tests for examples described in the GLIMMPSE paper.  
 REM
-java -cp "JavaStatistics.jar;junit-4.7.jar;commons-math-2.1.jar;jsc.jar" edu.cudenver.bios.power.test.paper.PaperResultsTestSuite > results.out
+java -cp "lib/JavaStatistics.jar;thirdparty/junit/4.7/lib/junit-4.7.jar;thirdparty/ApacheCommonsMath/2.1/lib/commons-math-2.1.jar;thirdparty/jsc/1.0/lib/jsc.jar" edu.cudenver.bios.power.test.paper.PaperResultsTestSuite > results.out
 
