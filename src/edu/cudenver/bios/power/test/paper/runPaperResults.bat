8aea10dfdc228325c1b617d789633457696d9fc8
Sarah Kreidler <sarah5280@gmail.com>
2012-12-04 22:45:39 +0000

    fixed path to javastatistics jar file in automated results run

--- src/edu/cudenver/bios/power/test/paper/runPaperResults.bat
+++ src/edu/cudenver/bios/power/test/paper/runPaperResults.bat
@@ -5,16 +5,16 @@ REM
 echo =====================
 echo Starting replication of Table 2. 
 echo Please note that simulations from this table may take several hours to complete.
-java -cp "lib/JavaStatistics.jar;thirdparty/junit/4.7/lib/junit-4.7.jar;thirdparty/ApacheCommonsMath/3.0/lib/commons-math3-3.0.jar;thirdparty/jsc/1.0/lib/jsc.jar" edu.cudenver.bios.power.test.paper.PaperTable2TestSuite > tables2.out
+java -cp "lib/edu.ucdenver.bios.javastatistics-UNKNOWN.jar;thirdparty/junit/4.7/lib/junit-4.7.jar;thirdparty/ApacheCommonsMath/3.0/lib/commons-math3-3.0.jar;thirdparty/jsc/1.0/lib/jsc.jar" edu.cudenver.bios.power.test.paper.PaperTable2TestSuite > tables2.out
 echo Done replicating results from Table 2.  Please see tables2.out for results.
 echo =====================
 echo Starting replication of Table 3. 
 echo Please note that simulations from this table may take several hours to complete.
-java -cp "lib/JavaStatistics.jar;thirdparty/junit/4.7/lib/junit-4.7.jar;thirdparty/ApacheCommonsMath/3.0/lib/commons-math3-3.0.jar;thirdparty/jsc/1.0/lib/jsc.jar" edu.cudenver.bios.power.test.paper.PaperTable3TestSuite > tables3.out
+java -cp "lib/edu.ucdenver.bios.javastatistics-UNKNOWN.jar;thirdparty/junit/4.7/lib/junit-4.7.jar;thirdparty/ApacheCommonsMath/3.0/lib/commons-math3-3.0.jar;thirdparty/jsc/1.0/lib/jsc.jar" edu.cudenver.bios.power.test.paper.PaperTable3TestSuite > tables3.out
 echo Done replicating results from Table 3.  Please see tables3.out for results.
 echo =====================
 echo Starting replication of power results from Figure 3.
-java -cp "lib/JavaStatistics.jar;thirdparty/junit/4.7/lib/junit-4.7.jar;thirdparty/ApacheCommonsMath/3.0/lib/commons-math3-3.0.jar;thirdparty/jsc/1.0/lib/jsc.jar" org.junit.runner.JUnitCore edu.cudenver.bios.power.test.paper.TestLawANOVA > figure3.out
+java -cp "lib/edu.ucdenver.bios.javastatistics-UNKNOWN.jar;thirdparty/junit/4.7/lib/junit-4.7.jar;thirdparty/ApacheCommonsMath/3.0/lib/commons-math3-3.0.jar;thirdparty/jsc/1.0/lib/jsc.jar" org.junit.runner.JUnitCore edu.cudenver.bios.power.test.paper.TestLawANOVA > figure3.out
 echo Done replicating results from Figure 4.  See figure4.out for results.
 
 REM
