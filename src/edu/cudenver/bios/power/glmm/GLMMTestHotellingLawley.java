7f671ec4173e5922c58ee0ddd378646fd931d361
Wes Munsil <wes_munsil@cytoanalytics.com>
2016-07-20 10:40:56 -0600

    White space changes.

--- src/edu/cudenver/bios/power/glmm/GLMMTestHotellingLawley.java
+++ src/edu/cudenver/bios/power/glmm/GLMMTestHotellingLawley.java
@@ -1,8 +1,8 @@
 /*
- * Java Statistics.  A java library providing power/sample size estimation for 
+ * Java Statistics.  A java library providing power/sample size estimation for
  * the general linear model.
- * 
- * Copyright (C) 2010 Regents of the University of Colorado.  
+ *
+ * Copyright (C) 2010 Regents of the University of Colorado.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -28,58 +28,58 @@ import edu.cudenver.bios.matrix.FixedRandomMatrix;
 /**
  * Implementation of the Hotelling Lawley Trace (HLT) test for the
  * general linear multivariate model
- * 
+ *
  * @author Sarah Kreidler
  *
  */
 public class GLMMTestHotellingLawley extends GLMMTest
-{    
-	/**
-	 * Create a Hotelling-Lawley Trace test object for the specified parameters
-	 * @param params GLMM input parameters
-	 */
+{
+    /**
+     * Create a Hotelling-Lawley Trace test object for the specified parameters
+     * @param params GLMM input parameters
+     */
     public GLMMTestHotellingLawley(FApproximation fMethod,
-    		RealMatrix Xessence, RealMatrix XtXInverse, int perGroupN, int rank,
-    		FixedRandomMatrix C, RealMatrix U, RealMatrix thetaNull, 
-    		RealMatrix beta, RealMatrix sigmaError)
+            RealMatrix Xessence, RealMatrix XtXInverse, int perGroupN, int rank,
+            FixedRandomMatrix C, RealMatrix U, RealMatrix thetaNull,
+            RealMatrix beta, RealMatrix sigmaError)
     {
         super(fMethod, Xessence, XtXInverse, perGroupN, rank,
-        		C, U, thetaNull, beta, sigmaError);
+                C, U, thetaNull, beta, sigmaError);
     }
-    
-	/**
-	 * Create a Hotelling-Lawley Trace test object for data analysis.  Used for
-	 * simulation.
-	 * @param params GLMM input parameters
-	 */
+
+    /**
+     * Create a Hotelling-Lawley Trace test object for data analysis.  Used for
+     * simulation.
+     * @param params GLMM input parameters
+     */
     public GLMMTestHotellingLawley(FApproximation fMethod,
-    		RealMatrix X, RealMatrix XtXInverse, int rank, RealMatrix Y,
-    		RealMatrix C, RealMatrix U, RealMatrix thetaNull)
+            RealMatrix X, RealMatrix XtXInverse, int rank, RealMatrix Y,
+            RealMatrix C, RealMatrix U, RealMatrix thetaNull)
     {
         super(fMethod, X, XtXInverse, rank, Y, C, U, thetaNull);
     }
-    
+
     /**
      * Calculate the denominator degrees of freedom for the HLT, based on
-     * whether the null or alternative hypothesis is assumed true.  
-     * 
+     * whether the null or alternative hypothesis is assumed true.
+     *
      * @param type distribution type
      * @return denominator degrees of freedom
      * @throws IllegalArgumentException
      */
     @Override
     public double getDenominatorDF(DistributionType type)
-    {       
+    {
         // a = #rows in between subject contrast matrix, C
         double a = (double) C.getRowDimension();
         // b = #columns in within subject contrast matrix
         double b = (double) U.getColumnDimension();
         // minimum of a and b dimensions
-        double s = (a < b) ? a : b;  
-        
+        double s = (a < b) ? a : b;
+
         double df = Double.NaN;
         if (fMethod == FApproximation.PILLAI_ONE_MOMENT ||
-        		fMethod == FApproximation.PILLAI_ONE_MOMENT_OMEGA_MULT)
+                fMethod == FApproximation.PILLAI_ONE_MOMENT_OMEGA_MULT)
         {
             df = s * ((totalN - rank) - b -1) + 2;
         }
@@ -95,8 +95,8 @@ public class GLMMTestHotellingLawley extends GLMMTest
 
     /**
      * Calculate the non-centrality parameter for the HLT, based on
-     * whether the null or alternative hypothesis is assumed true.  
-     * 
+     * whether the null or alternative hypothesis is assumed true.
+     *
      * @param type distribution type
      * @return non-centrality parameter
      * @throws IllegalArgumentException
@@ -107,7 +107,7 @@ public class GLMMTestHotellingLawley extends GLMMTest
         // calculate the hypothesis and error sum of squares matrices
         RealMatrix hypothesisSumOfSquares = getHypothesisSumOfSquares();
         RealMatrix errorSumOfSquares = getErrorSumOfSquares();
-                
+
         // check if we are uni or multi variate
         double p = beta.getColumnDimension();
         // a = #rows in between subject contrast matrix, C
@@ -115,10 +115,10 @@ public class GLMMTestHotellingLawley extends GLMMTest
         // b = #columns in within subject contrast matrix, U
         double b = U.getColumnDimension();
        // minimum of a and b dimensions
-        double s = (a < b) ? a : b;  
-        
+        double s = (a < b) ? a : b;
+
         double HLT = getHotellingLawleyTrace(hypothesisSumOfSquares, errorSumOfSquares);
-        
+
         if ((s == 1 && p > 1) ||
                 fMethod == FApproximation.PILLAI_ONE_MOMENT_OMEGA_MULT ||
                 fMethod == FApproximation.MCKEON_TWO_MOMENT_OMEGA_MULT)
@@ -134,8 +134,8 @@ public class GLMMTestHotellingLawley extends GLMMTest
 
     /**
      * Calculate the numerator degrees of freedom for the HLT, based on
-     * whether the null or alternative hypothesis is assumed true.  
-     * 
+     * whether the null or alternative hypothesis is assumed true.
+     *
      * @param type distribution type
      * @return numerator degrees of freedom
      * @throws IllegalArgumentException
@@ -145,21 +145,21 @@ public class GLMMTestHotellingLawley extends GLMMTest
     {
         double a = C.getRowDimension();
         double b = U.getColumnDimension();
-        
+
         return a * b;
     }
 
     /**
      * Calculate the observed F for the HLT, based on
-     * whether the null or alternative hypothesis is assumed true.  
-     * 
+     * whether the null or alternative hypothesis is assumed true.
+     *
      * @param type distribution type
      * @return observed F
      * @throws IllegalArgumentException
      */
     @Override
     public double getObservedF(DistributionType type)
-    {       
+    {
         if (type == DistributionType.DATA_ANALYSIS_NULL)
         {
             RealMatrix hypothesisSumOfSquares = getHypothesisSumOfSquares();
@@ -178,7 +178,7 @@ public class GLMMTestHotellingLawley extends GLMMTest
 
     /**
      * Compute a Hotelling-Lawley Trace statistic
-     * 
+     *
      * @param H hypothesis sum of squares matrix
      * @param E error sum of squares matrix
      * @returns F statistic
@@ -188,10 +188,10 @@ public class GLMMTestHotellingLawley extends GLMMTest
     {
         if (!H.isSquare() || !E.isSquare() || H.getColumnDimension() != E.getRowDimension())
             throw new IllegalArgumentException("Failed to compute Hotelling Lawley Trace: hypothesis and error matrices must be square and same dimensions");
-        
+
         RealMatrix inverseE = new LUDecomposition(E).getSolver().getInverse();
         RealMatrix HinverseE = H.multiply(inverseE);
-        
+
         return HinverseE.getTrace();
     }
 }
