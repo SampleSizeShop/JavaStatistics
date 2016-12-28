5088066550ff3e4b8cd9b8aa7be217d045c2cb23
Wes Munsil <wes_munsil@cytoanalytics.com>
2016-12-28 09:52:28 -0700

    White space changes.

--- src/edu/cudenver/bios/power/test/paper/TestConditionalUnivariateWithConfidenceLimits.java
+++ src/edu/cudenver/bios/power/test/paper/TestConditionalUnivariateWithConfidenceLimits.java
@@ -1,8 +1,8 @@
 /*
- * Java Statistics.  A java library providing power/sample size estimation for 
+ * Java Statistics.  A java library providing power/sample size estimation for
  * the general linear model.
- * 
- * Copyright (C) 2010 Regents of the University of Colorado.  
+ *
+ * Copyright (C) 2010 Regents of the University of Colorado.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -35,61 +35,61 @@ import edu.cudenver.bios.power.test.PowerChecker;
 import edu.cudenver.bios.power.test.ValidationReportBuilder;
 
 /**
- * Unit test for fixed univariate design with confidence intervals (powerlib example 4). 
+ * Unit test for fixed univariate design with confidence intervals (powerlib example 4).
  * Compared against simulation and SAS output.
- * 
+ *
  *  based on the example 4 from POWERLIB:
-*   Johnson J.L., Muller K.E., Slaughter J.C., Gurka M.J., Gribbin M.J. and Simpson S.L. 
-*   (2009) POWERLIB: SAS/IML software for computing power in multivariate linear models, 
+*   Johnson J.L., Muller K.E., Slaughter J.C., Gurka M.J., Gribbin M.J. and Simpson S.L.
+*   (2009) POWERLIB: SAS/IML software for computing power in multivariate linear models,
 *   Journal of Statistical Software, 30(5), 1-27.
- * 
+ *
  * @author Sarah Kreidler
  *
  */
 public class TestConditionalUnivariateWithConfidenceLimits extends TestCase
 {
-	private static final String DATA_FILE = "data" + File.separator + 
-	        "TestConditionalUnivariateWithConfidenceLimits.xml";
-	private static final String OUTPUT_FILE = "text" + File.separator + "results" + 
-	        File.separator + "TestConditionalUnivariateWithConfidenceLimits.tex";
-	private static final String TITLE = "GLMM(F) Example 4. Power and confidence limits for a univariate model";
-	   private static final String AUTHOR = "Sarah Kreidler";
-	    private static final String STUDY_DESIGN_DESCRIPTION  = 
-	            "The study design for Example 4 is a balanced two group design. " +
-	            "We calculate power for a two-sample t-test comparing the mean response " +
-	            "between the groups.  We calculate confidence limits for the power values. " +
-	            "The example is based on Figure 1 " +
-	            "from\n\n\\hangindent2em\n\\hangafter=1\n Taylor, D. J., \\& Muller, K. E. (1995). " +
-	            "Computing Confidence Bounds for Power and Sample Size of the " +
-	            "General Linear Univariate Model. \\emph{The American Statistician}, " +
-	            "\\emph{49}(1), 43-47.";
-	private PowerChecker checker;
-	
-	public void setUp()
-	{
-		try
-		{
-			checker = new PowerChecker(DATA_FILE, true);
-		}
-		catch (Exception e)
-		{
-			System.err.println("Setup failed: " + e.getMessage());
-			e.printStackTrace();
-			fail();
-		}
-	}
+    private static final String DATA_FILE = "data" + File.separator +
+            "TestConditionalUnivariateWithConfidenceLimits.xml";
+    private static final String OUTPUT_FILE = "text" + File.separator + "results" +
+            File.separator + "TestConditionalUnivariateWithConfidenceLimits.tex";
+    private static final String TITLE = "GLMM(F) Example 4. Power and confidence limits for a univariate model";
+       private static final String AUTHOR = "Sarah Kreidler";
+        private static final String STUDY_DESIGN_DESCRIPTION  =
+                "The study design for Example 4 is a balanced two group design. " +
+                "We calculate power for a two-sample t-test comparing the mean response " +
+                "between the groups.  We calculate confidence limits for the power values. " +
+                "The example is based on Figure 1 " +
+                "from\n\n\\hangindent2em\n\\hangafter=1\n Taylor, D. J., \\& Muller, K. E. (1995). " +
+                "Computing Confidence Bounds for Power and Sample Size of the " +
+                "General Linear Univariate Model. \\emph{The American Statistician}, " +
+                "\\emph{49}(1), 43-47.";
+    private PowerChecker checker;
+
+    public void setUp()
+    {
+        try
+        {
+            checker = new PowerChecker(DATA_FILE, true);
+        }
+        catch (Exception e)
+        {
+            System.err.println("Setup failed: " + e.getMessage());
+            e.printStackTrace();
+            fail();
+        }
+    }
 
     /**
-     * Compare 2 sample t-test results between JavaStatistics, 
+     * Compare 2 sample t-test results between JavaStatistics,
      * POWERLIB, and simulation
      */
     public void testUnviariateWithConfidenceLimits()
     {
         GLMMPowerParameters params = new GLMMPowerParameters();
-        
+
         // add tests
         params.addTest(Test.UNIREP);
-        
+
         // add alpha values
         params.addAlpha(0.01);
 
@@ -102,18 +102,18 @@ public class TestConditionalUnivariateWithConfidenceLimits extends TestCase
         // build theta null matrix
         double [][] theta0 = {{0}};
         params.setTheta(new Array2DRowRealMatrix(theta0));
-        
+
         // build sigma matrix
         double [][] sigma = {{0.068}};
         params.setSigmaError(new Array2DRowRealMatrix(sigma));
         // add sigma scale values
         params.addSigmaScale(1);
-        
+
         // build design matrix
         params.setDesignEssence(MatrixUtils.createRealIdentityMatrix(2));
         // add sample size multipliers
         params.addSampleSize(12);
-        
+
         // build between subject contrast
         double [][] between = {{1,-1}};
         params.setBetweenSubjectContrast(new FixedRandomMatrix(between, null, true));
@@ -122,7 +122,7 @@ public class TestConditionalUnivariateWithConfidenceLimits extends TestCase
         params.setConfidenceIntervalType(ConfidenceIntervalType.BETA_KNOWN_SIGMA_ESTIMATED);
         params.setSampleSizeForEstimates(24);
         params.setDesignMatrixRankForEstimates(2);
-        
+
         // run the test
         // 2 sided CI
         params.setAlphaLowerConfidenceLimit(0.025);
@@ -141,14 +141,14 @@ public class TestConditionalUnivariateWithConfidenceLimits extends TestCase
             ValidationReportBuilder reportBuilder = new ValidationReportBuilder();
             reportBuilder.createValidationReportAsStdout(checker, TITLE, false);
             reportBuilder.createValidationReportAsLaTex(
-                    OUTPUT_FILE, TITLE, AUTHOR, STUDY_DESIGN_DESCRIPTION, 
+                    OUTPUT_FILE, TITLE, AUTHOR, STUDY_DESIGN_DESCRIPTION,
                     params, checker);
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
 
-		assertTrue(checker.isSASDeviationBelowTolerance());
-		checker.reset();
+        assertTrue(checker.isSASDeviationBelowTolerance());
+        checker.reset();
     }
 
 }
